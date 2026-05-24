6c7bbda5e02729a6b70705715a9711d8e7fcca7d
Pelivan <Pelivan@DESKTOP-29KL5UV>
2026-05-24 14:23:23 +0300

    Default class prefix DONE

--- IPcalculator/main.cpp
+++ IPcalculator/main.cpp
@@ -29,17 +29,22 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			DWORD dwIPaddress = 0;
 			DWORD dwIPmask = 0;
+			DWORD dwIPprefix = 0;
 			HWND hIPaddress = GetDlgItem(hwnd, IDC_IP_ADDRESS);
 			HWND hIPmask = GetDlgItem(hwnd, IDC_IP_MASK);
+			HWND hIPprefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
 				SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
 				std::cout << FIRST_IPADDRESS(dwIPaddress) << std::endl;
-				if (FIRST_IPADDRESS(dwIPaddress) < 128)dwIPmask = 0xFF000000;
-				else if (FIRST_IPADDRESS(dwIPaddress) < 192)dwIPmask = 0xFFFF0000;
-				else if (FIRST_IPADDRESS(dwIPaddress) < 224)dwIPmask = 0xFFFFFF00;
+				if (FIRST_IPADDRESS(dwIPaddress) < 128)dwIPmask = 0xFF000000,dwIPprefix=8;
+				else if (FIRST_IPADDRESS(dwIPaddress) < 192)dwIPmask = 0xFFFF0000,dwIPprefix=16;
+				else if (FIRST_IPADDRESS(dwIPaddress) < 224)dwIPmask = 0xFFFFFF00,dwIPprefix=24;
 				std::cout << dwIPmask << std::endl;
 				SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
+				CHAR szIPprefix[3] = {};
+				sprintf(szIPprefix, "%i", dwIPprefix);
+				SendMessage(hIPprefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
 			}
 		}
 			break;
